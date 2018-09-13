7ce8330432f12f64902be56a39dd3fc57a1e62fd
Vera <verkaevg@mail.ru>
2018-09-13 17:06:02 +0400

    работа с формой по js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 var button = document.querySelector(".button-search");
   
   var popup = document.querySelector(".search-form");
+  
+  var arrival_date = popup.querySelector("[name=arrival_date]");
+  var departure_date = popup.querySelector("[name=departure_date]");
+  var people = popup.querySelector("[name=people]");
+  var children = popup.querySelector("[name=children]");
+
+  var isStorageSupport = true;
+  var storage = "";
+  
+  try {
+    storage = localStorage.getItem("arrival_date");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
   popup.classList.add("modal-hide");
   button.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("modal-hide");
+    arrival_date.focus();
   });
+  popup.addEventListener("submit", function (evt) {
+    if (!arrival_date.value || !departure_date.value||!people.value||!children.value){
+    evt.preventDefault();
+  } else {
+    if (isStorageSupport) {
+  localStorage.setItem("arrival_date", arrival_date.value);
+  localStorage.setItem("departure_date", departure_date.value);
+  localStorage.setItem("people", people.value);
+  localStorage.setItem("children", children.value);
+  }
+}
+  });
+
 
